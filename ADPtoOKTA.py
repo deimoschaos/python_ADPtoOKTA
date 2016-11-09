28809428b08f2437e42b86147c0aa46ec1caa147
Sam Johnson <psychoticpyro99@gmail.com>
2016-11-08 21:09:59 -0500

    updated login field to have '@something'

--- ADPtoOKTA.py
+++ ADPtoOKTA.py
@@ -4,26 +4,29 @@ from okta.models.user import User
 
 usersClient = UsersClient("https://flagshipcredit.oktapreview.com", "00NWlAD-Ftc1rYsT1-i044JTwa6FFopZkHvCyVrx5s")
 
+#Create the new user
 with open('sample.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		login  = (row['login'])
-		first  = (row['first'])
-		last   = (row['last'])
-		email  = (row['email'])
-		mobile = (row['mobile'])
+    reader = csv.DictReader(csvfile)
+    for row in reader:
 
-		user = User(login=login,
-			    email=email,
-			    firstName=first,
-			    lastName=last,
-			    mobilePhone=mobile)
+        first = (row['first'])
+        last = (row['last'])
+        login = (row['login'])
+        email = (row['email'])
+        mobileNum = (row['mobile'])
 
-		user = usersClient.create_user(user, activate=False)
+        user = User(login=login,
+                email=email,
+                firstName=first,
+                lastName=last,
+                mobilePhone=mobileNum)
 
+        user = usersClient.create_user(user, activate=False)
+
+#Make sure user was created
 #with open('sample.csv') as csvfile:
-#	reader = csv.DictReader(csvfile)
-#	for row in reader:
-#		login = (row['login'])
-#
-#		get_user(login)
+#    reader = csv.DictReader(csvfile)
+#    for row in reader:
+#        class okta.UsersClient("https://flagshipcredit.oktapreview.com", "00NWlAD-Ftc1rYsT1-i044JTwa6FFopZkHvCyVrx5s")
+#            login = (row['login'])
+#            get_user(login)
