a7fa36f1168b77b5ac967e3bd3ef369b9d9d9663
deimoschaos <psychoticpyro99@gmail.com>
2016-12-27 14:14:07 -0500

    Delete test.bash

--- test.bash
+++
@@ -1,5 +0,0 @@
-curl -v -X GET \
--H "Accept: application/json" \
--H "Content-Type: application/json" \
--H "Authorization: SSWS 00NWlAD-Ftc1rYsT1-i044JTwa6FFopZkHvCyVrx5s" \
-"https://flagshipcredit.oktapreview.com/api/v1/users/sam.johnson@flagshipcredit.com"
