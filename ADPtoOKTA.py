4b3d6d6891cebfb8d02187768a1ba13af34f04cb
Sam Johnson <psychoticpyro99@gmail.com>
2016-12-27 14:12:24 -0500

    Updated ADPtoOkta.py

--- ADPtoOKTA.py
+++ ADPtoOKTA.py
@@ -1,32 +1,132 @@
 import csv
 from okta import UsersClient
 from okta.models.user import User
+from okta import UserGroupsClient
+from okta.models.usergroup import UserGroup
+import os
+import io
+import time
+from datetime import date
 
-usersClient = UsersClient("https://flagshipcredit.oktapreview.com", "00NWlAD-Ftc1rYsT1-i044JTwa6FFopZkHvCyVrx5s")
+#Find new users
+def compareOldvNew():
 
-#Create the new user
-with open('sample.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
+    #Set key
+    column = 0
+    
+    #Set secondary key
+    secondary_column = 0
+    
+    #Open CSV files
+    master = open('file1.csv', 'rb')
+    secondary = open('file2.csv', 'rb')
+    results = open('results.csv', 'wb')
+    
+    #Get header in CSV
+    header = master.next()
 
-        first = (row['first'])
-        last = (row['last'])
-        login = (row['login'])
-        email = (row['email'])
-        mobileNum = (row['mobile'])
+    #Splitting off the header
+    for idx, label in enumerate(header.split(',')):
+        if label == "Position ID":
+            column = idx
+            
+    keys = {}
+    
+    #Sets Employee ID as the key in Master CSV
+    for line in master:
+        keys.update({line.split(',')[column]:None})
+        print keys
+    
+    #Sets Employee ID as the key in Secondary CSV
+    secondary_header = secondary.next()
+    for idx, label in enumerate(secondary_header.split(',')):
+        if label == 'Position ID':
+            secondary_column = idx
+            
+    #Writes Header to results.csv
+    results.write(header)
+    
+    #Finds the any new users in the latest CSV file
+    for line in secondary:
+        if line.split(',')[secondary_column] in keys:
+            #Do nothing because the ID is in the master
+            print "ID exists in master CSV:", line.split(',')[secondary_column]
+        else:
+            #Key is not in the master. Put whole row in the results
+            print "New ID Found:", line.split(',')[secondary_column]
+            results.write(line)
+    
+    #Closes all CSV files
+    master.close()
+    secondary.close()
+    results.close()
+    return 
+    
+#Creates the curl bash file for each new Okta User
+def createBashFile():
 
-        user = User(login=login,
-                email=email,
-                firstName=first,
-                lastName=last,
-                mobilePhone=mobileNum)
+    with open('results.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        iteration=1
+        for row in reader:
 
-        user = usersClient.create_user(user, activate=False)
+            firstName       = (row['First Name'])
+            lastName        = (row['Last Name'])
+            middleName      = (row['Middle Name'])
+            email           = (row['Work Contact: Work Email'])
+            title           = (row['Job Title Description'])
+            primaryPhone    = (row['Work Contact: Work Phone'])
+            city            = (row['Location Description'])
+            employeeNumber  = (row['Position ID'])
+            department      = (row['Home Department Description'])
+            manager         = (row['Reports To Name'])
+            positionStatus  = (row['Position Status'])
+            termDate        = (row['Termination Date'])
+            login           = firstName + "." + lastName + "@companyname.com"
+            
+            fileName = "createUser" + str(iteration) + ".bash"
+            with io.FileIO(fileName, "w") as file:
+                f = file.write
+                f("curl -v -X POST \\\n")
+                f("-H \"Accept: application/json\" \\\n")
+                f("-H \"Content-Type: application/json\" \\\n")
+                f("-H \"Authorization: SSWS ${API TOKEN}\" \\\n")
+                f("-d '{\n")
+                f("  \"profile\": {\n")
+                f("    \"login\": \"" + login + "\",\n")
+                f("    \"firstName\": \"" + firstName + "\",\n")
+                f("    \"lastName\": \"" + lastName + "\",\n")
+                f("    \"middleName\": \"" + middleName + "\",\n")
+                f("    \"email\": \"" + email + "\",\n")
+                f("    \"title\": \"" + title + "\",\n")
+                f("    \"primaryPhone\": \"" + primaryPhone + "\",\n")
+                f("    \"city\": \"" + city + "\",\n")
+                f("    \"employeeNumber\": \"" + employeeNumber + "\",\n")
+                f("    \"department\": \"" + department + "\",\n")
+                f("    \"manager\": \"" + manager + "\"\n")
+                f("  }\n")
+                f("}' \"https://${ORG}.oktapreview.com/api/v1/users?activate=false\"")
+                
+            iteration +=1
 
-#Make sure user was created
-#with open('sample.csv') as csvfile:
-#    reader = csv.DictReader(csvfile)
-#    for row in reader:
-#        class okta.UsersClient("https://flagshipcredit.oktapreview.com", "00NWlAD-Ftc1rYsT1-i044JTwa6FFopZkHvCyVrx5s")
-#            login = (row['login'])
-#            get_user(login)
+#Runs the bash file and pushes the new user to Okta via curl
+def runBashFile():
+
+    os.system("chmod +x createUser*")
+    os.system("for i in createUser*; do bash $i; done")
+
+#Cleans up all the bash files
+def cleanup():
+
+    d = date.fromordinal(730920) # 730920th day after 1. 1. 0001
+    currentDate = d.strftime("%d%m%y")
+    currentTime = time.strftime("%Ih%Mm%Ss")
+    newFolder = "mkdir userArchive/" + currentDate + "_" + currentTime
+    mvFiles = "mv createUser* userArchive/" + currentDate + "_" + currentTime
+    os.system(newFolder)
+    os.system(mvFiles)
+
+compareOldvNew()
+createBashFile()
+runBashFile()
+cleanup()
